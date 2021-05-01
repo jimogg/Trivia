70febda3ed9ce5f399c57a61094e2cae2fceaf5d
jimogg <jnogana@gmail.com>
2021-05-01 12:14:58 -0400

    Created questions.js file to hold trivia questions.

---
+++ questions.js
@@ -0,0 +1,34 @@
+quesCatArray = [
+    {
+        question: "This is the first question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0000
+    },
+    {
+        question: "This is the second question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0000
+    },
+    {
+        question: "This is the third question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0000
+    },
+    {
+        question: "This is the fourth question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 000
+    }
+]
