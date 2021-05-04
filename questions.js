53226ce4922cc6fe7129f75accfff4035813281f
jimogg <jnogana@gmail.com>
2021-05-04 16:19:23 -0400

    More dummy question arrays.

--- questions.js
+++ questions.js
@@ -1,9 +1,124 @@
-quesCatArray = [
+quesCatArray0 = [
     {
         categoryName: "U.S. Presidents"
     },
     {
-        question: "This is the first question about something interesting.",
+        question: "This is the first question CAT0 something interesting. And now I shall add more text to show the appearance of the div wrap.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0100
+    },
+    {
+        question: "This is the second question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0010
+    },
+    {
+        question: "This is the third question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0001
+    },
+    {
+        question: "This is the fourth question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 1000
+    }
+]
+
+
+quesCatArray1 = [
+    {
+        categoryName: "U.S. Presidents"
+    },
+    {
+        question: "This is the first question CAT1 something interesting. And now I shall add more text to show the appearance of the div wrap.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0100
+    },
+    {
+        question: "This is the second question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0010
+    },
+    {
+        question: "This is the third question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0001
+    },
+    {
+        question: "This is the fourth question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 1000
+    }
+]
+
+quesCatArray2 = [
+    {
+        categoryName: "U.S. Presidents"
+    },
+    {
+        question: "This is the first question CAT2 something interesting. And now I shall add more text to show the appearance of the div wrap.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0100
+    },
+    {
+        question: "This is the second question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0010
+    },
+    {
+        question: "This is the third question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 0001
+    },
+    {
+        question: "This is the fourth question about something interesting.",
+        choiceA: "This is chocie A",
+        choiceB: "This is chocie B",
+        choiceC: "This is chocie C",
+        choiceD: "This is chocie D",
+        key: 1000
+    }
+]
+
+quesCatArray3 = [
+    {
+        categoryName: "U.S. Presidents"
+    },
+    {
+        question: "This is the first question CAT3 something interesting. And now I shall add more text to show the appearance of the div wrap.",
         choiceA: "This is chocie A",
         choiceB: "This is chocie B",
         choiceC: "This is chocie C",
